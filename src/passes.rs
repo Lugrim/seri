c3a39502d3f38683c37c22dfe6094e384dfdd8b9
Camille Le Bon <camille.lebon35000@gmail.com>
2023-03-28 19:31:02 +0200

    Make CompilingPass generic over the input only
    The return type and error type are supposedly known when defining the
    pass. Thus they can be defined as associated types and not
    generics. This does not change much to the code of compilation passes,
    but it makes pass chaining a lot easier. Instead of using the
    silverfish operator with 3 generic parameters (the pass type, the
    return type, the error type), it is now used with only one parameter
    that is the pass type. The return and error types are determined using
    the pass' Residual and Error associated types.

--- src/passes.rs
+++ src/passes.rs
@@ -5,23 +5,31 @@ pub mod parser;
 
 /// A trait defining compilation passes
 /// Compilation passes should be chainable
-pub trait CompilingPass<T, U, UE> {
+pub trait CompilingPass<T> {
+    /// Type of data that is returned by the pass if it succeeds.
+    type Residual;
+    /// Type of errors that the pass returns when it fails.
+    type Error;
+
     /// Transforms a given input of type T to a given output of type T
     ///
     /// # Errors
     ///
     /// May return errors if type is not compatible
-    fn apply(_: T) -> Result<U, UE>;
+    fn apply(_: T) -> Result<Self::Residual, Self::Error>;
 }
 
 /// A trait to be able to chain compilation passes
-pub trait PassInput : Sized {
+pub trait PassInput: Sized {
     /// Chain current element with a given compilation pass
     ///
     /// # Errors
     ///
     /// Can return a fitting error if a compilation pass cannot be applied.
-    fn chain_pass<P, U, E>(self) -> Result<U, E> where P: CompilingPass<Self, U, E> {
+    fn chain_pass<P>(self) -> Result<P::Residual, P::Error>
+    where
+        P: CompilingPass<Self>,
+    {
         P::apply(self)
     }
 }
