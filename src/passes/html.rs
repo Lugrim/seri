cf2b31cf472166899a4beaea1fd3937b824ad25e
Hugo <hugo.reymond@mailo.fr>
2023-04-07 17:41:31 +0200

    Use contextual passes to provide parameters to the HTML backend
    Removed the old shared variable "OPTIONS" and moved to a contextual pass.

--- src/passes/html.rs
+++ src/passes/html.rs
@@ -1,7 +1,7 @@
 //! HTML backend
 use crate::{event::Event, passes::CompilingPass};
 use chrono::Timelike;
-use std::{cmp::min, str::FromStr, sync::Mutex};
+use std::{cmp::min, str::FromStr};
 use thiserror::Error;
 
 /// Backend outputing events to a standalone HTML document containing a timetable
@@ -18,11 +18,6 @@ pub struct HTMLBackendOptions {
     pub template_path: Option<String>,
 }
 
-/// Options of the HTML backend
-static OPTIONS: Mutex<HTMLBackendOptions> = Mutex::new(HTMLBackendOptions {
-    template_path: None,
-});
-
 /// Error that can occur during the compilation of the HTML backend
 #[derive(Debug, Error)]
 pub enum HTMLBackendCompilationError {
@@ -34,9 +29,9 @@ pub enum HTMLBackendCompilationError {
     CouldNotReadTemplate(#[from] std::io::Error),
 }
 
-fn get_template() -> Result<String, std::io::Error> {
-    let template_path = OPTIONS.lock().unwrap().template_path.clone();
-    match template_path.as_ref() {
+#[allow(clippy::option_if_let_else)]
+fn get_template(template_path: Option<String>) -> Result<String, std::io::Error> {
+    match template_path {
         None => Ok(include_str!("../../data/template.html").to_string()),
         Some(path) => std::fs::read_to_string(path),
     }
@@ -58,24 +53,37 @@ impl ToHTML for Event {
     }
 }
 
-impl HTMLBackend {
-    /// Configure the HTML backend
-    ///
-    /// # Arguments
-    ///
-    /// * `opts` - The options to use, see [`HTMLBackendOptions`]
-    ///
-    pub fn configure(opts: HTMLBackendOptions) {
-        *OPTIONS.lock().unwrap() = opts;
+impl CompilingPass<Vec<Event>> for HTMLBackend {
+    type Residual = String;
+    type Error = HTMLBackendCompilationError;
+    fn apply(events: Vec<Event>) -> Result<Self::Residual, Self::Error> {
+        Self::apply_with(
+            events,
+            HTMLBackendOptions {
+                template_path: None,
+            },
+        )
     }
 }
 
-impl CompilingPass<Vec<Event>> for HTMLBackend {
+impl CompilingPass<Vec<Event>, HTMLBackendOptions> for HTMLBackend {
     type Residual = String;
     type Error = HTMLBackendCompilationError;
 
     fn apply(events: Vec<Event>) -> Result<Self::Residual, Self::Error> {
-        let template = get_template()?;
+        Self::apply_with(
+            events,
+            HTMLBackendOptions {
+                template_path: None,
+            },
+        )
+    }
+
+    fn apply_with(
+        events: Vec<Event>,
+        options: HTMLBackendOptions,
+    ) -> Result<Self::Residual, Self::Error> {
+        let template = get_template(options.template_path)?;
         let mut str = String::new();
         str.push_str("<table><tbody>\n");
         for i in 9..21 {
