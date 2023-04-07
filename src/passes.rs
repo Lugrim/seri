24b0ba93f7c979098e12f74f8d7f7a3b741a020d
Amélie "Lymkwi" Gonzalez <lymkwi@vulpinecitrus.info>
2023-04-07 16:19:46 +0200

    Implement compiling passes with context structures
    Add an `apply_with` method to the `CompilingPass` trait such that pass
    inputs can be called with `apply_with` and an object containing
    associated context/values.
    
    This commit is a proof of work to replace the use of a mutex and global
    variable in #12 .
    
    In order to modify that pull request to use it, please apply the patch
    below.
    
    Signed-off-by: Amélie "Lymkwi" Gonzalez <lymkwi@vulpinecitrus.info>
    
    ---
    
    diff --git a/src/main.rs b/src/main.rs
    index 2a569f1..7a34ec2 100644
    --- a/src/main.rs
    +++ b/src/main.rs
    @@ -89,10 +89,9 @@ fn generate_html(
         options: HTMLBackendOptions,
         content: &str,
     ) -> Result<String, HTMLBackendCompilationError> {
    -    HTMLBackend::configure(options);
         content
             .chain_pass::<ParseTimetable>()?
    -        .chain_pass::<HTMLBackend>()
    +        .chain_pass_with::<HTMLBackend, HTMLBackendOptions>(options)
     }
    
     /// Write the output to a file or to stdout
    diff --git a/src/passes/html.rs b/src/passes/html.rs
    index 415f885..14cc854 100644
    --- a/src/passes/html.rs
    +++ b/src/passes/html.rs
    @@ -1,22 +1,27 @@
     //! HTML backend
     use crate::{event::Event, passes::CompilingPass};
     use chrono::Timelike;
    -use std::{cmp::min, str::FromStr, sync::Mutex};
    +use std::{cmp::min, str::FromStr};
     use thiserror::Error;
    
     /// Backend outputing events to a standalone HTML document containing a timetable
     pub struct HTMLBackend {}
    
     /// Options for the HTML backend
    +#[derive(Default)]
     pub struct HTMLBackendOptions {
         /// Path to the template file. If not set, the default template (`data/template.html`) will be used.
         pub template_path: Option<String>,
     }
    
    -/// Options of the HTML backend
    -static OPTIONS: Mutex<HTMLBackendOptions> = Mutex::new(HTMLBackendOptions {
    -    template_path: None,
    -});
    +impl HTMLBackendOptions {
    +    fn get_template(&self) -> Result<String, std::io::Error> {
    +        match self.template_path.as_ref() {
    +            None => Ok(include_str!("../../data/template.html").to_string()),
    +            Some(path) => std::fs::read_to_string(path),
    +        }
    +    }
    +}
    
     /// Error that can occur during the compilation of the HTML backend
     #[derive(Debug, Error)]
    @@ -29,14 +34,6 @@ pub enum HTMLBackendCompilationError {
         CouldNotReadTemplate(#[from] std::io::Error),
     }
    
    -fn get_template() -> Result<String, std::io::Error> {
    -    let options = OPTIONS.lock().unwrap();
    -    match options.template_path.as_ref() {
    -        None => Ok(include_str!("../../data/template.html").to_string()),
    -        Some(path) => std::fs::read_to_string(path),
    -    }
    -}
    -
     fn event_to_string(event: &Event) -> String {
         let duration = std::cmp::max(event.duration / 30, 1);
         let class = event.event_type.to_string();
    @@ -51,19 +48,19 @@ fn event_to_string(event: &Event) -> String {
         res
     }
    
    -impl HTMLBackend {
    -    /// Configure the HTML backend
    -    pub fn configure(opts: HTMLBackendOptions) {
    -        *OPTIONS.lock().unwrap() = opts;
    -    }
    -}
    -
    -impl CompilingPass<Vec<Event>> for HTMLBackend {
    +impl CompilingPass<Vec<Event>, HTMLBackendOptions> for HTMLBackend {
         type Residual = String;
         type Error = HTMLBackendCompilationError;
    
         fn apply(events: Vec<Event>) -> Result<Self::Residual, Self::Error> {
    -        let template = get_template()?;
    +        Self::apply_with(events, HTMLBackendOptions::default())
    +    }
    +
    +    fn apply_with(
    +        events: Vec<Event>,
    +        options: HTMLBackendOptions,
    +    ) -> Result<Self::Residual, Self::Error> {
    +        let template = options.get_template()?;
             let mut str = String::new();
             str.push_str("<table><tbody>\n");
             for i in 9..21 {

--- src/passes.rs
+++ src/passes.rs
@@ -6,7 +6,7 @@ pub mod parser;
 
 /// A trait defining compilation passes
 /// Compilation passes should be chainable
-pub trait CompilingPass<T> {
+pub trait CompilingPass<T, C=()> {
     /// Type of data that is returned by the pass if it succeeds.
     type Residual;
     /// Type of errors that the pass returns when it fails.
@@ -18,6 +18,17 @@ pub trait CompilingPass<T> {
     ///
     /// May return errors if type is not compatible
     fn apply(_: T) -> Result<Self::Residual, Self::Error>;
+
+    /// Transforms a given input of type T to a given output of type T given a
+    /// context
+    ///
+    /// # Errors
+    ///
+    /// May return errors if type is not compatible
+    fn apply_with(data: T, _: C) -> Result<Self::Residual, Self::Error> {
+        // The default implementation provided discards any context
+        Self::apply(data)
+    }
 }
 
 /// A trait to be able to chain compilation passes
@@ -32,5 +43,14 @@ pub trait PassInput: Sized {
         P: CompilingPass<Self>,
     {
         P::apply(self)
+    }
+
+    /// Chain current element with a given compilation pass with context data
+    ///
+    /// # Errors
+    ///
+    /// Can return a fitting error if a compilation pass cannot be applied.
+    fn chain_pass_with<P: CompilingPass<Self, U>, U>(self, ctx: U) -> Result<P::Residual, P::Error> {
+        P::apply_with(self, ctx)
     }
 }
